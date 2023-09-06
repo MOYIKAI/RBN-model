e27e3785a09f5d19cac0adf01c8df285a813e613
MOYIKAI <119965788+MOYIKAI@users.noreply.github.com>
2023-09-05 23:06:11 -0500

    Add files via upload

--- Mutual.c
+++ Mutual.c
@@ -1,111 +1,44 @@
-#include <stdio.h>
+// Adjascency List representation in C
 #include <stdbool.h>
+#include <stdio.h>
 #include <stdlib.h>
-#include "my_nrutil.h"
+#include "my_nrutil.h"// include NR header files
 
-#define MAX_NODES 40000
-
-// Structure to represent a node in the graph
-struct Node {
-    int data;
-    struct Node* next;
-};
-
-// Structure to represent the graph
-struct Graph {
-    int numNodes;
-    struct Node* adjList[MAX_NODES];
-};
-
-// Function to initialize a graph
-void initializeGraph(struct Graph* graph, int numNodes) {
-    graph->numNodes = numNodes;
-    for (int i = 0; i < numNodes; i++) {
-        graph->adjList[i] = NULL;
-    }
-}
-
-// Function to add an edge to the graph
-void addEdge(struct Graph* graph, int src, int dest) {
-    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
-    newNode->data = dest;
-    newNode->next = graph->adjList[src];
-    graph->adjList[src] = newNode;
-}
-
-// Helper function for finding all cycles
-void findCycles(struct Graph* graph, int v, bool visited[], int path[], int pathLength, int numc) {
-    visited[v] = true;
-    path[pathLength] = v;
-    pathLength++;
-
-    struct Node* neighbor = graph->adjList[v];
-    while (neighbor != NULL) {
-        int neighborNode = neighbor->data;
-        if (!visited[neighborNode]) {
-            findCycles(graph, neighborNode, visited, path, pathLength, numc);
-        }
-        else {
-            
-            // Check if the neighbor is part of the current path (cycle)
-            for (int i = 0; i < pathLength; i++) {
-                if (path[i] == neighborNode) {
-                    numc++;
-                    printf("nc%d ", numc);
-                    printf("pl%d ", pathLength - 1);
-                    for (int j = i; j < pathLength; j++) {
-                        printf("%d ", path[j]);
-                    }
-                    printf("%d\n", neighborNode);
-                }
-            }
-        }
-        neighbor = neighbor->next;
-    }
-
-}
-
-// Function to find and print all cycles in the graph
-void findAllCycles(struct Graph* graph) {
-    bool visited[MAX_NODES] = {false};
-    int path[MAX_NODES];
-    int numc; // numbers of cycle
-    for (int i = 0; i < graph->numNodes; i++) {
-        if (visited[i] == false){
-            findCycles(graph, i, visited, path, 0, numc);
-        }
-    }
-}
 
 int main(int argc, char *argv[]) {
-    struct Graph graph;
-    int numNodes, numEdges;
-    
-    FILE *cf;	// input configuration file
 
-    if (argc == 4){
-        // require argc be equal to number of command line entries
-        numNodes = atol(argv[1]);
-        numEdges = atol(argv[2]);
-    } 
-    else {
-        // error input value of argc 
-        fprintf(stderr,"\n Initialization error:\n");
-        fprintf(stderr,"Usage: Mutual.x N totaledges Functiongraph\n");
-        return 1;
-    }
+  int data,i;
+  int clength,cnum; // cycle length
+  FILE *cf;   // input configuration file
 
-    initializeGraph(&graph, numNodes);
-    
-    cf = fopen(argv[3],"r");
-    for (int i = 0; i < numEdges; i++) {
-        int src, dest;
-        
-        fscanf(cf, "%d %d", &src, &dest);
-        addEdge(&graph, src, dest);
+  if (argc == 2){
+  // require argc be equal to number of command line entries
+  }
+  else {
+    // error input value of argc
+    fprintf(stderr,"\n Initialization error:\n");
+    fprintf(stderr,"Usage: Mutual.x Cycledata\n");
+    return 1;
+  }
+  // Fisrt time to read the data info
+  cnum=0;
+  cf = fopen(argv[1],"r");
+  while(fscanf(cf,"%d", &clength) != EOF){
+    printf("cycle length %d \n",clength);
+    for (i=0; i<clength; i++){
+      fscanf(cf,"%d", &data);
+      printf("%d ",data);
     }
-    
-    findAllCycles(&graph);
-    
-    return 0;
+    printf("\n");
+    cnum++;
+  }
+  fclose(cf);
+
+  cf = fopen(argv[1],"r");
+  while(fscanf(cf,"%d",&clength) != EOF){
+    printf("%d \n",clength);
+  }
+  fclose(cf);
+  return 0;
+
 }
