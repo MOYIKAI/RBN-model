81935d4944d3297a6858dc58a70eca363227f6d4
MOYIKAI <119965788+MOYIKAI@users.noreply.github.com>
2023-11-16 12:00:51 -0600

    Delete random.h

--- random.h
+++
@@ -1,7 +0,0 @@
-#include <string.h>   // standard string library
-#include <stdio.h>	  // standard io library
-#include <stdlib.h>	  // standard library with lots of functions
-#include <math.h>	  // standard math library
-
-float ran1(long *idum);
-float ran2(long *idum);
